b735b94e378829dab66528385fef973594b76206
BharatSH25 <bharatharikant25@gmail.com>
2026-03-12 18:20:31 +0530

    Defined inteface for different managed vector db

--- app/rag/utils.py
+++ app/rag/utils.py
@@ -4,3 +4,8 @@ from __future__ import annotations
 def to_pgvector(values: list[float]) -> str:
     # pgvector expects a string like "[0.1,0.2,0.3]"
     return "[" + ",".join(f"{v:.6f}" for v in values) + "]"
+
+# Need to implement interface for different vectordb's
+def to_pinecone(values: list[float]):
+    #TODO : to implement pincone
+    return values
